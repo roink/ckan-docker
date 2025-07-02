301d8704d8a2403eb0588bcbb619098569582d3e
philipp <sproink@gmx.de>
2025-07-02 11:57:29 +0000

    Discard default search facets; Create Custom theme plugin

--- ckan/ckanext-customfacets/ckanext/customfacets/plugin.py
+++ ckan/ckanext-customfacets/ckanext/customfacets/plugin.py
@@ -7,6 +7,10 @@ class CustomFacetsPlugin(plugins.SingletonPlugin):
 
     def dataset_facets(self, facets_dict, package_type):
         # keep existing facets, then add ours
+        facets_dict.clear()
         facets_dict['subject']              = toolkit._('Data Pillar')
         facets_dict['language']             = toolkit._('Language') 
+        facets_dict['keywords']             = toolkit._('Keywords')
+        facets_dict['rights']             = toolkit._('License')
+        facets_dict['format']             = toolkit._('File Type')
         return facets_dict
