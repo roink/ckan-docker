c732fed91dde6e54cd4e7dc7afe0cc13c6573e47
philipp <sproink@gmx.de>
2025-07-04 08:53:57 +0000

    Revert feature development to prepare clean branching

--- ckan/ckanext-customfacets/ckanext/customfacets/plugin.py
+++ ckan/ckanext-customfacets/ckanext/customfacets/plugin.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
-import json
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 class CustomFacetsPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IFacets)
-    plugins.implements(plugins.IPackageController, inherit=True)
 
     def dataset_facets(self, facets_dict, package_type):
         # keep existing facets, then add ours
         facets_dict['subject']              = toolkit._('Data Pillar')
-        facets_dict['language']             = toolkit._('Language')
-        facets_dict['keywords']             = toolkit._('Keywords')
-        facets_dict['rights']             = toolkit._('License')
-        facets_dict['format']             = toolkit._('File Type')
-        facets_dict['geologicalTimeframe'] = toolkit._('Geological Timeframe')
         return facets_dict
 
     # CKAN will also call these when rendering group or organization pages.
@@ -24,34 +17,3 @@ class CustomFacetsPlugin(plugins.SingletonPlugin):
 
     def group_facets(self, facets_dict, group_type, package_type):
         return self.dataset_facets(facets_dict, package_type)
-
-    def before_index(self, data_dict):
-        """Extract geological timeframe values for faceting."""
-        # Values might already be a list or a JSON string
-        raw = data_dict.get('geologicalTimeframes')
-
-        if not raw:
-            # Look for the extras entry if the field is stored there
-            for extra in data_dict.get('extras', []):
-                if extra.get('key') == 'geologicalTimeframes':
-                    raw = extra.get('value')
-                    break
-
-        if isinstance(raw, str):
-            try:
-                raw = json.loads(raw)
-            except Exception:
-                raw = []
-
-        values = []
-        if isinstance(raw, list):
-            for entry in raw:
-                if isinstance(entry, dict):
-                    tf = entry.get('geologicalTimeframe')
-                    if tf:
-                        values.append(tf)
-
-        if values:
-            data_dict['geologicalTimeframe'] = values
-
-        return data_dict
